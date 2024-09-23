945f020fa98f6c1b84cbaa4374e93b07160d0a2f
biswajeet787 <biswajeet780@gmail.com>
2024-09-23 14:29:51 +0530

    first commit

---
+++ src/assets/images/img.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const img = () => {
+  return (
+    <View>
+      <Text>img</Text>
+    </View>
+  )
+}
+
+export default img
+
+const styles = StyleSheet.create({})
---
+++ src/assets/svg/svg.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const svg = () => {
+  return (
+    <View>
+      <Text>svg</Text>
+    </View>
+  )
+}
+
+export default svg
+
+const styles = StyleSheet.create({})
