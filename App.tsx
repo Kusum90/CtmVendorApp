bb338db10d6e323be842d517b144a4835dbbd0e9
biswajeet787 <biswajeet780@gmail.com>
2024-09-23 17:57:48 +0530

    first push

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import RootNavigation from './src/navigation/RootNavigation'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{flex:1,backgroundColor:'orange'}}>
+      <RootNavigation/>
     </View>
   )
 }
---
+++ src/navigation/RootNavigation.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import TabNavigation from './TabScreen/TabNavigation'
+
+const RootNavigation = () => {
+  return (
+    <View>
+      <TabNavigation/>
+    </View>
+  )
+}
+
+export default RootNavigation
