6678531e4fe01fba388b84e66659dc45a4a80bc6
silan-halli <silanhalli01@gmail.com>
2024-10-17 19:12:18 +0530

    HeaderScreen

---
+++ src/screens/MediaScreen/MediaScreen.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { ScrollView } from 'react-native-gesture-handler'
+
+import Media from '../../components/ContentComponent/Media'
+
+const MediaScreen = () => {
+  return (
+    <ScrollView>
+     
+      <Media/>
+    </ScrollView>
+  )
+}
+
+export default MediaScreen
--- src/screens/OrdersScreen/OrderScreen.jsx
+++ src/screens/OrdersScreen/OrderScreen.jsx
@@ -10,7 +10,7 @@ const OrderScreen = () => {
   return (
     <ScrollView>
         <Dashboard/>
-        <Order/>
+        {/* <Order/> */}
         <OrdersData/>
     </ScrollView>
   )
