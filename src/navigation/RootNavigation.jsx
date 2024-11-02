caf4d0c90fe2a1dfbc21a7820d3157209500514c
biswajeet787 <biswajeet780@gmail.com>
2024-11-02 11:36:36 +0530

    splash

--- src/navigation/RootNavigation.jsx
+++ src/navigation/RootNavigation.jsx
@@ -3,14 +3,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DrawerNavigation from './TabScreen/TabNavigation';
 import StackNavigation from './StackScreen/StackNavigation';
+import SplashScreen from '../screens/SplashScreen';
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='SplashScreen'>
         {/* DrawerNavigation which includes the TabNavigation */}
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
         <Stack.Screen name="DrawerNavigation" component={DrawerNavigation} />
         <Stack.Screen name="Stack" component={StackNavigation} />
       </Stack.Navigator>
---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import { scaleWidth, scaleHeight } from '../utils/responsiveUtils';
+
+const SplashScreen = ({ navigation }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace('DrawerNavigation');
+    }, 2000);
+
+    return () => clearTimeout(timer); 
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../assets/images/SplashScreen/CleanTechMartLogo.png')}
+          style={styles.logo}
+        />
+        <Image
+          source={require('../assets/images/SplashScreen/splashImage.png')}
+          style={styles.animation}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: scaleWidth(350),
+    height: scaleHeight(165),
+    resizeMode: 'contain',
+  },
+  animation: {
+    width: scaleWidth(225),
+    height: scaleHeight(325),
+    resizeMode: 'contain',
+  },
+});
+
+export default SplashScreen;
